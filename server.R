1041a26939e1303d12ccefe57f115bc4df885d4a
Mateusz Czarnecki <mateusz.czarnecki14@gmail.com>
2022-07-18 21:49:55 +0200

    add temporary variable selection view

--- server.R
+++ server.R
@@ -1,16 +1,33 @@
 library(data.table)
 library(randomForest)
 
+data_table_options <- list(
+  pageLength = 10,
+  scrollX = TRUE
+)
+
 shinyServer(function(input, output, session) {
-  output$contents <- renderDataTable({
-    input_file <- input$csv_file
-    if (is.null(input_file)) {
-      return(NULL)
-    }
-    read.csv(input_file$datapath, header = input$header)
-  },
-    options = list(
-    pageLength = 10,
-    scrollX = TRUE
-  ))
+  output$contents <- renderDataTable(
+    {
+      input_file <- input$csv_file
+      if (is.null(input_file)) {
+        return(NULL)
+      }
+      data <- read.csv(input_file$datapath, header = input$header)
+      print(data)
+    },
+    options <- data_table_options
+  )
+
+  output$contents_selection <- renderDataTable(
+    {
+      input_file <- input$csv_file
+      if (is.null(input_file)) {
+        return(NULL)
+      }
+      data <- read.csv(input_file$datapath, header = input$header)
+      print(data)
+    },
+    options <- data_table_options
+  )
 })
--- ui.R
+++ ui.R
@@ -2,32 +2,62 @@ library(shiny)
 library(shinythemes)
 library(markdown)
 
-fluidPage(theme = shinytheme("united"),
-  navbarPage(
-    "Variable Selection Application:",
-    tabPanel("Select file",
-      sidebarPanel(
-        fileInput("csv_file", "Choose CSV File",
-          accept = c(
-            "text/csv",
-            "text/comma-separated-values,text/plain",
-            ".csv")
-          ),
-        tags$hr(),
-        checkboxInput("header", "Header", TRUE)
-      ),
-      mainPanel(
-        dataTableOutput("contents")
+app_title <- "Variable Selection Application:"
+
+file_selection_tab_panel <- tabPanel(
+  "Select file",
+  sidebarPanel(
+    fileInput("csv_file", "Choose CSV File",
+      accept = c(
+        "text/csv",
+        "text/comma-separated-values,text/plain",
+        ".csv"
       )
     ),
-    tabPanel("Variable selection method",
-    "This panel is intentionally left blank"),
-    tabPanel("Classification method",
-    "This panel is intentionally left blank"),
-    tabPanel("Perform and see results",
-    "This panel is intentionally left blank"),
-    tabPanel("About",
-    div(includeMarkdown("about.md"),
-    align = "justify")),
+    checkboxInput("header", "Header", TRUE)
+  ),
+  mainPanel(
+    dataTableOutput("contents")
+  )
+)
+
+selection_method_tab_panel <- tabPanel(
+  "Variable selection method",
+  sidebarPanel(
+    radioButtons(
+      "dist", "Choose selection method:",
+      c(
+        "No variable selection" = "no_selection",
+        "PCA" = "pca",
+        "BMA" = "bma"
+      )
+    ),
+    actionButton("selection_button", "Perform selection")
+  ),
+  mainPanel(
+    dataTableOutput("contents_selection")
+  )
+)
+
+fluidPage(
+  theme = shinytheme("united"),
+  navbarPage(
+    app_title,
+    file_selection_tab_panel,
+    selection_method_tab_panel,
+    tabPanel(
+      "Classification method",
+      "This panel is intentionally left blank"
+    ),
+    tabPanel(
+      "Perform and see results",
+      "This panel is intentionally left blank"
+    ),
+    tabPanel(
+      "About",
+      div(includeMarkdown("about.md"),
+        align = "justify"
+      )
+    ),
   ),
 )
