500989d26b201077cc01009b2a4fc18ed5e48356
Mateusz Czarnecki <mateusz.czarnecki14@gmail.com>
2022-07-19 20:59:09 +0200

    codestyle

--- ui/methodSelectionTabPanel.R
+++ ui/methodSelectionTabPanel.R
@@ -1,13 +1,15 @@
+selection_methods <- c(
+  "No variable selection" = "no_selection",
+  "PCA" = "pca",
+  "BMA" = "bma"
+)
+
 method_selection_tab_panel <- tabPanel(
   "Variable selection method",
   sidebarPanel(
     radioButtons(
       "dist", "Choose selection method:",
-      c(
-        "No variable selection" = "no_selection",
-        "PCA" = "pca",
-        "BMA" = "bma"
-      )
+      selection_methods,
     ),
     actionButton("selection_button", "Perform selection")
   ),
